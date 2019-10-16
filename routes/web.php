cc26d0586fae1ae81107c273789b226f1ad7ab00
Daniel <careca_esquivel@hotmail.com>
2019-10-15 23:10:21 -0300

    Se modificó las rutas.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,27 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
 });
+
+Route::get('/usuarios', function () {
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/nuevo', function () {
+    return 'Crear nuevo Usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "Mostrando detalles del usuario: {$id}";
+})->where('id', '[0-9]+');
+
+Route::get('/saludo/{nombre}/{apodo?}', function ($nombre, $apodo = null) {
+    if($apodo) {
+        return "Bienvenido {$nombre}, tu apodo es {$apodo}";
+    } else {
+        return "Bienvenido {$nombre}, no tienes apodo";
+    }
+    
+});
+
